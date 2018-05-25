9b66bb2abaa2674c03bdb629d096b8d4260db027
Son Nguyen <sonnguyen@macbook-pro-2.local>
2018-05-25 14:46:10 +1000

    Initial Commit

---
+++ CrashalyticsPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrashalyticsPOC
+//
+//  Created by Son Nguyen on 25/5/18.
+//  Copyright © 2018 Son Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
